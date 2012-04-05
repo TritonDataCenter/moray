596c54f3922a85709c2bf92a0d0ac803b34dbf72
Mark Cavage <mcavage@gmail.com>
2012-04-05 15:35:21 -0700

    Make dev script create database moray, rather than test

--- dev_env.sh
+++ dev_env.sh
@@ -1,7 +1,7 @@
 export MORAY_URL=http://localhost:8080
 export PATH=$PWD/build/node/bin:$PWD/node_modules/.bin:$PATH
 
-alias newdb='dropdb test; createdb test'
+alias newdb='dropdb moray; createdb moray'
 alias test_db='newdb && nodeunit ./test/db/*.test.js 3>&1 1>&2 2>&3 | bunyan'
 alias server='node main.js -f ./etc/moray.development.config.json 2>&1 -d | bunyan'
 
