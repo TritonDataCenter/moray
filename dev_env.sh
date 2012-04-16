9339e93b57583b50d6bca617fd09d7255aac97c9
Mark Cavage <mcavage@gmail.com>
2012-04-16 16:10:16 -0700

    Better search compilation

--- dev_env.sh
+++ dev_env.sh
@@ -4,7 +4,7 @@ export PATH=$PWD/build/node/bin:$PWD/node_modules/.bin:$PATH
 alias newdb='dropdb moray; createdb moray'
 alias test_db='newdb && nodeunit ./test/db/*.test.js 3>&1 1>&2 2>&3 | bunyan'
 alias server='node main.js -f ./etc/moray.development.config.json 2>&1 -d | bunyan'
-alias moray_laptop='newdb && node main.js -f ./etc/moray.laptop.config.json -d 1 2>&1 | bunyan'
+alias moray_laptop='newdb && node main.js -f ./etc/moray.laptop.config.json 2>&1 | bunyan'
 
 function mcurl() {
     /usr/bin/curl -isk \
