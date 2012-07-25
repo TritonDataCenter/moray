8814cfce50ab964aac8fcf9ff73f99e7e0175cc7
Mark Cavage <mark.cavage@joyent.com>
2012-07-25 04:25:07 +0000

    MANTA-63: checkpoint on node-fast

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var assert = require('assert-plus');
 var bunyan = require('bunyan');
 var clone = require('clone');
 var getopt = require('posix-getopt');
-var restify = require('restify');
 var extend = require('xtend');
 
 var app = require('./lib');
@@ -21,14 +20,19 @@ var DEFAULTS = {
         file: process.cwd() + '/etc/config.json',
         fork: true,
         logLevel: bunyan.WARN,
-        port: 80
+        port: 1234
 };
 var NAME = 'moray';
 var LOG = bunyan.createLogger({
         name: NAME,
         level: (process.env.LOG_LEVEL || 'warn'),
         stream: process.stderr,
-        serializers: restify.bunyan.serializers
+        serializers: {
+                err: bunyan.stdSerializers.err,
+                pg: function (client) {
+                        return (client ? client._moray_id : undefined);
+                }
+        }
 });
 
 
@@ -126,138 +130,9 @@ _config = readConfig(_options);
 LOG.debug({config: _config}, 'configuration loaded');
 
 if (cluster.isMaster && _config.fork) {
-        var numCPUs = (os.cpus().length - 1 || 1);
+        var numCPUs = process.env.MORAY_THREADS || (os.cpus().length - 1 || 1);
         for (var i = 0; i < numCPUs; i++)
                 cluster.fork();
 } else {
         run(_config);
 }
-
-
-
-/*
-
-
-
-var NAME = 'moray';
-var DEFAULT_CFG = __dirname + '/etc/' + NAME + '.config.json';
-var LOG;
-var PARSED;
-var DTP;
-var SERVERS = [];
-
-var OPTS = {
-    'debug': Number,
-    'file': String,
-    'port': Number,
-    'help': Boolean
-};
-
-var SHORT_OPTS = {
-    'd': ['--debug'],
-    'f': ['--file'],
-    'p': ['--port'],
-    'h': ['--help']
-};
-
-
-
-///--- Internal Functions
-
-function usage(code, message) {
-    var _opts = '';
-    Object.keys(SHORT_OPTS).forEach(function (k) {
-        var longOpt = SHORT_OPTS[k][0].replace('--', '');
-        var type = OPTS[longOpt].name || 'string';
-        if (type && type === 'boolean') type = '';
-        type = type.toLowerCase();
-
-        _opts += ' [--' + longOpt + ' ' + type + ']';
-    });
-
-    var msg = (message ? message + '\n' : '') +
-        'usage: ' + path.basename(process.argv[1]) + _opts;
-
-    console.error(msg);
-    process.exit(code);
-}
-
-
-function run(callback) {
-    var opts = {
-        file: PARSED.file || DEFAULT_CFG,
-        overrides: PARSED,
-        log: LOG,
-        dtrace: DTP
-    };
-
-    return app.createServer(opts, function (err, server) {
-        if (err) {
-            console.error('Unable to create server: %s', err.message);
-            process.exit(1);
-        }
-
-        server.start(function () {
-            SERVERS.push(server);
-            LOG.info('%s listening at %s', NAME, server.url);
-            if (typeof (callback) === 'function')
-                return callback();
-            return false;
-        });
-    });
-}
-
-
-function startREPL() {
-    net.createServer(function (socket) {
-        var r = repl.start('moray> ', socket);
-        r.context.SERVERS = SERVERS;
-    }).listen(5001, 'localhost', function () {
-        LOG.info('REPL started on 5001');
-    });
-}
-
-
-
-///--- Mainline
-
-PARSED = nopt(OPTS, SHORT_OPTS, process.argv, 2);
-if (PARSED.help)
-    usage(0);
-
-DTP = d.createDTraceProvider(NAME);
-LOG = new Logger({
-    level: PARSED.debug ? 'debug' : 'info',
-    name: NAME,
-    stream: process.stderr,
-    serializers: {
-        err: Logger.stdSerializers.err,
-        req: Logger.stdSerializers.req,
-        res: restify.bunyan.serializers.response
-    }
-});
-
-if (PARSED.debug) {
-    if (PARSED.debug > 1)
-        LOG.level('trace');
-
-    run(startREPL());
-} else if (cluster.isMaster) {
-    for (var i = 0; i < os.cpus().length - 1; i++)
-        cluster.fork();
-
-    cluster.on('death', function (worker) {
-        LOG.error({worker: worker}, 'worker %d exited');
-        cluster.fork();
-    });
-
-    startREPL();
-} else {
-    run();
-}
-
-process.on('uncaughtException', function (err) {
-    LOG.fatal({err: err}, 'uncaughtException handler (exiting error code 1)');
-    process.exit(1);
-});
-*/
