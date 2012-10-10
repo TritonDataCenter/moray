68db746a5cda05788781e5088e519ac1a42857e0
Mark Cavage <mark.cavage@joyent.com>
2012-10-10 19:02:36 +0000

    MANTA-328: take two - hack up ldap search filter to support numerics

--- test/integ.test.js
+++ test/integ.test.js
@@ -128,3 +128,93 @@ test('MANTA-328 numeric values in filters', function (t) {
                 });
         });
 });
+
+
+test('MANTA-328 numeric values in filters <=', function (t) {
+        var b = this.bucket;
+        var c = this.client;
+        var k = uuid();
+        var cfg = {
+                index: {
+                        num: {
+                                type: 'number'
+                        }
+                }
+        };
+        var data = {
+                num: 425
+        };
+
+        c.putBucket(b, cfg, function (err1) {
+                t.ifError(err1);
+                c.putObject(b, k, data, function (err2) {
+                        t.ifError(err2);
+                        var ok = false;
+                        var f = '(num<=1024)';
+                        var req = c.findObjects(b, f);
+                        req.once('error', function (err) {
+                                t.ifError(err);
+                                t.end();
+                        });
+                        req.once('end', function () {
+                                t.ok(ok);
+                                t.end();
+                        });
+                        req.once('record', function (obj) {
+                                t.ok(obj);
+                                t.equal(obj.bucket, b);
+                                t.equal(obj.key, k);
+                                t.deepEqual(obj.value, data);
+                                t.ok(obj._id);
+                                t.ok(obj._etag);
+                                t.ok(obj._mtime);
+                                ok = true;
+                        });
+                });
+        });
+});
+
+
+test('MANTA-328 numeric values in filters >=', function (t) {
+        var b = this.bucket;
+        var c = this.client;
+        var k = uuid();
+        var cfg = {
+                index: {
+                        num: {
+                                type: 'number'
+                        }
+                }
+        };
+        var data = {
+                num: 425
+        };
+
+        c.putBucket(b, cfg, function (err1) {
+                t.ifError(err1);
+                c.putObject(b, k, data, function (err2) {
+                        t.ifError(err2);
+                        var ok = false;
+                        var f = '(num>=81)';
+                        var req = c.findObjects(b, f);
+                        req.once('error', function (err) {
+                                t.ifError(err);
+                                t.end();
+                        });
+                        req.once('end', function () {
+                                t.ok(ok);
+                                t.end();
+                        });
+                        req.once('record', function (obj) {
+                                t.ok(obj);
+                                t.equal(obj.bucket, b);
+                                t.equal(obj.key, k);
+                                t.deepEqual(obj.value, data);
+                                t.ok(obj._id);
+                                t.ok(obj._etag);
+                                t.ok(obj._mtime);
+                                ok = true;
+                        });
+                });
+        });
+});
