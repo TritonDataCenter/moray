fdffced63d442f079c2fb1a85c114b4e8503b578
Mark Cavage <mark.cavage@joyent.com>
2012-10-16 17:18:18 +0000

    MANTA-218: update to latest version of fast (piggyback: unbreak verror WError names)

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 export PATH=$PWD/build/node/bin:$PWD/node_modules/.bin:node_modules/moray/bin:$PATH
 
-alias moray='node main.js -f ./etc/config.bh1-kvm9.json -v -s 2>&1 | bunyan'
+alias server='node main.js -f ./etc/config.coal.json -v -s 2>&1 | bunyan'
+alias npm='node `which npm`'
