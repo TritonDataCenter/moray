a7803fcadc61598a408d3d44839ccca1ec139a6e
Pedro P. Candel <pedro@joyent.com>
2013-08-06 13:22:45 +0200

    [minor] Updated test-env setup due to MORAY-160 changes

--- tools/coal-test-env.sh
+++ tools/coal-test-env.sh
@@ -29,14 +29,15 @@ MORAY_TEST_SMF=/opt/smartdc/moray/smf/manifests/moray-test.xml
 
 zlogin $MORAY "cp /opt/smartdc/moray/etc/config.json /opt/smartdc/moray/etc/config.test.json"
 
-zlogin $MORAY "cp /opt/smartdc/moray/smf/manifests/moray.xml /opt/smartdc/moray/smf/manifests/moray-test.xml"
+zlogin $MORAY "cp /opt/smartdc/moray/smf/manifests/moray-2021.xml /opt/smartdc/moray/smf/manifests/moray-test.xml"
 
 zlogin $MORAY "/opt/local/bin/gsed -i -e \"s|smartdc-moray|smartdc-moray-test|\" $MORAY_TEST_SMF"
 
 zlogin $MORAY "/opt/local/bin/gsed -i -e \"s|smartdc/application/moray|smartdc/application/moray-test|\" $MORAY_TEST_SMF"
 
-zlogin $MORAY "/opt/local/bin/gsed -i -e \"s|2020|2222|\" $MORAY_TEST_SMF"
+zlogin $MORAY "/opt/local/bin/gsed -i -e \"s|2021|2222|\" $MORAY_TEST_SMF"
 
 zlogin $MORAY "/opt/local/bin/gsed -i -e \"s|value=\\\"/usr/lib/extendedFILE.so.1\\\" />|value=\\\"/usr/lib/extendedFILE.so.1\\\" /><envvar name=\\\"MORAY_DB_NAME\\\" value=\\\"moray_test\\\" />|\" $MORAY_TEST_SMF"
 
 zlogin $MORAY "/usr/sbin/svccfg import /opt/smartdc/moray/smf/manifests/moray-test.xml; /usr/sbin/svcadm enable moray-test"
+
