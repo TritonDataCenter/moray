ccb80c9a07b01b8e8aaf88541af9bd83e176f1dd
Mark Cavage <mcavage@gmail.com>
2013-08-15 15:28:01 +0000

    MORAY-142: array type support

--- test/arrays.test.js
+++ test/arrays.test.js
@@ -93,3 +93,218 @@ test('schema array, array value (string)', function (t) {
                 t.end();
         });
 });
+
+
+test('schema array, scalar value (string)', function (t) {
+        var b = this.bucket;
+        var c = this.client;
+        var k = uuid();
+        var cfg = {
+                index: {
+                        name: {
+                                type: '[string]',
+                                unique: false
+                        }
+                }
+        };
+        var data = {
+                name: 'foo',
+                ignoreme: 'foo'
+        };
+
+        vasync.pipeline({
+                funcs: [
+                        function bucket(_, cb) {
+                                c.putBucket(b, cfg, cb);
+                        },
+                        function object(_, cb) {
+                                c.putObject(b, k, data, cb);
+                        },
+                        function find(_, cb) {
+                                cb = once(cb);
+
+                                var found = false;
+                                var req = c.findObjects(b, '(name=foo)');
+                                req.once('error', cb);
+                                req.once('record', function (obj) {
+                                        t.ok(obj);
+                                        if (obj) {
+                                                t.equal(obj.bucket, b);
+                                                t.equal(obj.key, k);
+                                                // Moray converts back into an
+                                                // array...
+                                                data.name = [data.name];
+                                                t.deepEqual(obj.value, data);
+                                                t.ok(obj._id);
+                                                t.ok(obj._etag);
+                                                t.ok(obj._mtime);
+                                                found = true;
+                                        }
+                                });
+                                req.once('end', function () {
+                                        t.ok(found);
+                                        cb();
+                                });
+                        }
+                ],
+                arg: null
+        }, function (err) {
+                t.ifError(err);
+                t.end();
+        });
+});
+
+
+test('schema array, array value (number)', function (t) {
+        var b = this.bucket;
+        var c = this.client;
+        var k = uuid();
+        var cfg = {
+                index: {
+                        id: {
+                                type: '[number]',
+                                unique: false
+                        }
+                }
+        };
+        var data = {
+                id: [1, 2, 3],
+                ignoreme: 'foo'
+        };
+        var found = 0;
+
+        function checkObject(obj) {
+                t.ok(obj);
+                if (obj) {
+                        t.equal(obj.bucket, b);
+                        t.equal(obj.key, k);
+                        t.deepEqual(obj.value, data);
+                        t.ok(obj._id);
+                        t.ok(obj._etag);
+                        t.ok(obj._mtime);
+                        found++;
+                }
+        }
+
+        vasync.pipeline({
+                funcs: [
+                        function setup(_, cb) {
+                                c.putBucket(b, cfg, function (err) {
+                                        if (err) {
+                                                cb(err);
+                                        } else {
+                                                c.putObject(b, k, data, cb);
+                                        }
+                                });
+                        },
+                        function eq(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(id=1)');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function gte(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(id>=3)');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function lte(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(id<=1)');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function presence(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(id=*)');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function and(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(&(id<=3)(id>=1))');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function or(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(|(id<=0)(id>=1))');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        },
+                        function not(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(!(id=0))');
+                                req.once('error', cb);
+                                req.once('record', checkObject);
+                                req.once('end', cb);
+                        }
+                ],
+                arg: null
+        }, function (err, results) {
+                t.ifError(err);
+                t.equal(found, results.operations.length - 1);
+                t.end();
+        });
+});
+
+
+test('schema array string, substring filter throws', function (t) {
+        var b = this.bucket;
+        var c = this.client;
+        var k = uuid();
+        var cfg = {
+                index: {
+                        name: {
+                                type: '[string]',
+                                unique: false
+                        }
+                }
+        };
+        var data = {
+                name: ['foo', 'bar', 'baz'],
+                ignoreme: 'foo'
+        };
+
+        vasync.pipeline({
+                funcs: [
+                        function setup(_, cb) {
+                                c.putBucket(b, cfg, function (err) {
+                                        if (err) {
+                                                cb(err);
+                                        } else {
+                                                c.putObject(b, k, data, cb);
+                                        }
+                                });
+                        },
+                        function substr(_, cb) {
+                                cb = once(cb);
+
+                                var req = c.findObjects(b, '(name=f*)');
+                                req.once('error', function (err) {
+                                        t.ok(err);
+                                        cb(err);
+                                });
+                                req.once('end', cb);
+                        }
+                ],
+                arg: null
+        }, function (err, results) {
+                t.ok(err);
+                t.end();
+        });
+});
