090d4a764cba06596a2a5554097c67976f342749
David Pacheco <dap@joyent.com>
2017-01-30 18:20:38 -0800

    MORAY-393 document moray env file

--- env.sh
+++ env.sh
@@ -8,6 +8,12 @@
 # Copyright (c) 2017, Joyent, Inc.
 #
 
+#
+# This environment file is intended for use only in development environments.
+# This should set up any environment variables or aliases that are helpful for
+# running Moray directly out of this workspace.  See the README for details.
+#
+
 set -o xtrace
 export PATH=$PWD/build/node/bin:$PWD/node_modules/.bin:node_modules/moray/bin:$PATH
 alias npm='node `which npm`'
